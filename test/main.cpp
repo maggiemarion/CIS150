10a4499c644dfe7d2f709ba178a9c15d24bab93c
Christian Munte <cmunte@Christians-MacBook.local>
2015-09-16 14:19:10 -0400

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Christian Munte on 9/16/15.
+//  Copyright (c) 2015 Christian Munte. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
