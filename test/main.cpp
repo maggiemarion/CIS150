8878ca98203ffe71d99f3396e1492435765b0442
chris munte <chris_munte@yahoo.com>
2015-11-05 14:26:22 -0500

    Initial

--- test/main.cpp
+++ test/main.cpp
@@ -7,9 +7,14 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#define read "/Users/cmunte/Google Drive/MacLocal/CIS/test/test/file.txt"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+int main() {
+    float exam;
+    float x=23,y=30;
+    exam = (x/y)*100;
+    cout << exam;
+    return 0; }
